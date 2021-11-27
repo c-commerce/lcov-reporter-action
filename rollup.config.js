a1cf108d7e81c349e1064c80cedc8284d5b94890
Robert Jefe Lindstaedt <robert.lindstaedt@gmail.com>
2021-11-27 12:41:10 +0100

    chore: renovate

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,26 @@
-import commonjs from "@rollup/plugin-commonjs"
-import resolve from "@rollup/plugin-node-resolve"
-import json from "@rollup/plugin-json"
-import externals from "rollup-plugin-node-externals"
+/* eslint-disable node/no-unpublished-import */
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import json from '@rollup/plugin-json'
+import externals from 'rollup-plugin-node-externals'
 
 export default {
-	input: "src/index.js",
-	output: {
-		file: "dist/main.js",
-		format: "cjs",
-	},
-	treeshake: true,
-	plugins: [
-		externals({
-			builtin: true,
-			deps: false,
-		}),
-		resolve({
-			preferBuiltins: true,
-			mainFields: [ "main" ],
-		}),
-		commonjs(),
-		json(),
-	],
+  input: 'src/index.js',
+  output: {
+    file: 'dist/main.js',
+    format: 'cjs'
+  },
+  treeshake: true,
+  plugins: [
+    externals({
+      builtin: true,
+      deps: false
+    }),
+    resolve({
+      preferBuiltins: true,
+      mainFields: ['main']
+    }),
+    commonjs(),
+    json()
+  ]
 }
--- src/cli.js
+++ src/cli.js
@@ -1,36 +1,37 @@
-import process from "process"
-import { promises as fs } from "fs"
-import path from "path"
+import process from 'process'
+import { promises as fs } from 'fs'
+import path from 'path'
 
-import { parse } from "./lcov"
-import { diff } from "./comment"
+import { parse } from './lcov'
+import { diff } from './comment'
 
-async function main() {
-	const file = process.argv[2]
-	const beforeFile = process.argv[3]
-	const prefix = path.dirname(path.dirname(path.resolve(file))) + "/"
+async function main () {
+  const file = process.argv[2]
+  const beforeFile = process.argv[3]
+  const prefix = path.dirname(path.dirname(path.resolve(file))) + '/'
 
-	const content = await fs.readFile(file, "utf-8")
-	const lcov = await parse(content)
+  const content = await fs.readFile(file, 'utf-8')
+  const lcov = await parse(content)
 
-	let before
-	if (beforeFile) {
-		const content = await fs.readFile(beforeFile, "utf-8")
-		before = await parse(content)
-	}
+  let before
+  if (beforeFile) {
+    const content = await fs.readFile(beforeFile, 'utf-8')
+    before = await parse(content)
+  }
 
-	const options = {
-		repository: "example/foo",
-		commit: "f9d42291812ed03bb197e48050ac38ac6befe4e5",
-		prefix,
-		head: "feat/test",
-		base: "master",
-	}
+  const options = {
+    repository: 'example/foo',
+    commit: 'f9d42291812ed03bb197e48050ac38ac6befe4e5',
+    prefix,
+    head: 'feat/test',
+    base: 'master'
+  }
 
-	console.log(diff(lcov, before, options))
+  console.log(diff(lcov, before, options))
 }
 
-main().catch(function(err) {
-	console.log(err)
-	process.exit(1)
+main().catch(function (err) {
+  console.log(err)
+  // eslint-disable-next-line no-process-exit
+  process.exit(1)
 })
